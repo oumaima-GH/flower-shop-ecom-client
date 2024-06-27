8bcf7189be72ab2c0c2f24e43c3e95b4ef7e843f
oumaima-GH <oumaima.elmamouni97@gmail.com>
2024-06-27 12:49:55 +0100

    fixed the navbar while scrolling and the hero section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Header.css';
 
 function Header() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.getElementById('navbar');
+      if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+      } else {
+        header.classList.remove('scrolled');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
+    <header className="header" id="navbar">
       <a href="#" className='logo'>Flower Shop</a>
       <nav>
         <ul>
-          <li><a href="#home">Home</a></li>
+          <li><a href="#">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#shop">Shop</a></li>
           <li><a href="#contact">Contact</a></li>
